eff97d3c8ee72bf25dbefd2104814fe6917df863
aberrantian <aberrantian@gmail.com>
2022-11-03 00:44:46 +0000

    retrieve button value on click

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ function divide(a, b) {
 function operate(a, b, c) {
     return a(b, c);
 };
+
+let userInput = '';
+let inputText = document.querySelector('.input-text');
+let outputText = document.querySelector('.output-text');
+/*
+Create nodelist of number buttons
+For each node
+    Add event listener for click
+    When click > copy node's text content into userInput
+*/
+
+const numberButtons = document.querySelectorAll('.number');
+for (const button of numberButtons) {
+    button.addEventListener('click', () => {
+        console.log(button.textContent);
+    });
+};
